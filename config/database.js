b77b18578184e1b355fe953f39cc9faf9dd8d43a
shxdowlynnie <thirdeyebikes@gmail.com>
2026-08-04 17:56:17 -0300

    Crear config/database.js

---
+++ config/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export async function connectDatabase() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log("🟢 MongoDB conectado correctamente.");
+
+    } catch (error) {
+
+        console.error("🔴 Error al conectar MongoDB.");
+        console.error(error);
+
+        process.exit(1);
+    }
+}
